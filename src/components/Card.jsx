4c594e306ef168e40fabb49579080238d611d291
Regis Cater <olg.mel@gmail.com>
2023-11-07 21:46:41 +0100

    toggle du modifying Historical note

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -84,8 +84,6 @@ export default function Card({ title, eventDate, status, owner, description, car
   * Effacer une note de l'historique
   **************************************************************************/
   const deleteHistorical = async (docId) => {
-
-
     try {
       await deleteDoc(doc(db, "subjects", cardId, "historical", docId))
       //console.log("Deleting : ", docId);
@@ -119,6 +117,13 @@ export default function Card({ title, eventDate, status, owner, description, car
 
     setModifyingHistorical(undefined)
     setUpdating(undefined);
+  }
+
+  /**************************************************************************
+  * Ouvrir ou ferme le forumlaire de modification d'une note de l'historique
+  **************************************************************************/
+  const toggleModifyingHistorical = (docId) => {
+    modifyingHistorical !== docId ? setModifyingHistorical(docId) : setModifyingHistorical(undefined)
   }
 
   /**************************************************************************
@@ -196,7 +201,7 @@ export default function Card({ title, eventDate, status, owner, description, car
                       <div className="justify-end items-center mr-2">
 
                         {/* Bouton MODIFY */}
-                        <button className="group/edit" onClick={() => setModifyingHistorical(historicalDoc.id)}>
+                        <button className="group/edit" onClick={() => toggleModifyingHistorical(historicalDoc.id)}>
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 ml-2 text-transparent group-hover:text-blue-500 group-hover/edit:text-blue-700">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                           </svg>
